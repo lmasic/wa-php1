60614dbad85dc76b8873700c40885d1f2bc7f9be
lm85 <lmweb@centrum.cz>
2024-05-13 13:07:09 +0200

    mysql

--- send.php
+++ send.php
@@ -1,6 +1,15 @@
-<?php 
+<?php  //send.php
+require "db.php";
 if (isset($_POST["jm"]))
-    print_r($_POST);
+  { // print_r($_POST);
+  
+    //TODO: ošetøit mysql injection
+   
+    $mysqli -> query("insert into form set jm='".$_POST["jm"]."',zprava='".$_POST["zprava"]."'");
+
+    
+   } 
+    
     
     header("Location: http://lm/?id=kontakt&x=ok");
 ?>
